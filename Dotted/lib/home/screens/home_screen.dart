f83045e5b3ab2daeaa2a10a67fed2e8a1328fe37
lcqff <yeonnu82g@gmail.com>
2024-04-25 15:54:38 +0900

    feat: 로그인 기능 개발
    #1

---
+++ Dotted/lib/home/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    print("홈 화면");
+    throw UnimplementedError();
+  }
+
+}
--- Dotted/lib/login/screens/login_screen.dart
+++ Dotted/lib/login/screens/login_screen.dart
@@ -1,17 +1,18 @@
+import 'dart:convert';
+
 import 'package:dotted/login/service/login_service.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted/login/utils/login_platform.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
+import '../../home/screens/home_screen.dart';
 import '../widget/login_button.dart';
 import '../widget/logout_button.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
-  printlog() {
-    print("로그인화면");
-  }
 
   @override
   State<StatefulWidget> createState() => _LoginScreenState();
@@ -24,38 +25,65 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print("로그인 화면");
+
     return Scaffold(
       body: Center(
         child: _loginPlatform != LoginPlatform.none
             ? LogoutButton(
                 loginPlatform: _loginPlatform,
                 onLogout: _handleLogout,
-               )
+              )
             : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            LoginButton(
-              imagePath: 'kakao_login',
-              onTap: _signInWithKakao
-            ),
-          ],
-        ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LoginButton(
+                      imagePath: 'kakao_login', onTap: _signInWithKakao),
+                ],
+              ),
       ),
     );
   }
 
-  void _signInWithKakao() {
-    _loginService.signInWithKakao(_loginPlatform, _setLoginPlatform);
+  Future<void> _signInWithKakao() async {
+    try {
+      final clientState = Uuid().v4();
+      final url = Uri.https('kauth.kakao.com', '/oauth/authorize', {
+        'response_type': 'code',
+        'client_id': dotenv.env['KAKAO_NATIVE_APP_KEY'],
+        'redirect_uri': dotenv.env['REDIRECT_URL'],
+        'state': clientState,
+      });
+
+      final result = await FlutterWebAuth.authenticate(
+          url: url.toString(), callbackUrlScheme: "webauthcallback");
+
+      final body = Uri.parse(result).queryParameters;
+
+      final Map<String, dynamic> responseBody = json.decode(body as String);
+      final userId = responseBody['userId'];
+      final token = responseBody['token'];
+
+    } catch (error) {
+      print('카카오톡 로그인 실패 $error');
+    }
   }
 
   void _handleLogout(LoginPlatform platform) {
     _loginService.signOut(platform, _setLoginPlatform);
   }
 
-
   void _setLoginPlatform(LoginPlatform platform) {
     setState(() {
       _loginPlatform = platform;
     });
   }
+}
+
+void _handleLoginResponse(BuildContext context) {
+  Navigator.pop(context);
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => HomeScreen()),
+  );
 }
